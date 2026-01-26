ce15e2ff7c898222c2025a359e32b120281f638e
AyoVizzion69 <AyoVizzion@protonmail.com>
2026-01-26 14:56:40 -0600

    Refactor server.js to use environment PORT and add route

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const { readFileSync, writeFileSync } = require('fs');
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(5000, () => console.log('http://localhost:5000/'))
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express ");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Express listening on ${PORT}`);
+});
