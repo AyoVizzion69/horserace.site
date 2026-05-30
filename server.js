4851fb0afe7734775dd64a0b79b4425d83e44653
vizzion <AyoVizzion@protonmail.com>
2026-05-30 01:25:11 -0500

    Remove emoji from response message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Express + Caddy 🚀");
+  res.send("Hello from Express + Caddy ");
 });
 
 app.listen(PORT, "127.0.0.1", () => {
